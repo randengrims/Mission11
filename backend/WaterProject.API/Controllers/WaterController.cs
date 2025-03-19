34d2e1b64a3650f9eda0e9513b632197511f0ffb
randengrims <randengrimshaw@gmail.com>
2025-03-18 18:22:27 -0600

    initial commit

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDBContext _waterContext;
+        public WaterController(WaterDBContext temp) => _waterContext = temp;
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> GetProjects()
+        {
+            return _waterContext.Projects.ToList();
+        }
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var something = _waterContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+            return something;
+        }
+    }
+}
---
+++ backend/WaterProject.API/Data/WaterDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WaterProject.API.Data
+{
+    public class WaterDBContext : DbContext 
+    {
+        public WaterDBContext(DbContextOptions<WaterDBContext> options) : base(options) { }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
