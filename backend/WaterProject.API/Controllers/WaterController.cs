8513d050599cad902250196d952435b46044629d
randengrims <randengrimshaw@gmail.com>
2025-03-20 21:16:39 -0600

    BC Mission 11

--- backend/WaterProject.API/Controllers/WaterController.cs
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -13,6 +13,17 @@ namespace WaterProject.API.Controllers
         [HttpGet("AllProjects")]
         public IActionResult GetProjects(int pageSize = 10, int pageNum = 1)
         {
+            string? favProjType = Request.Cookies["favoriteProjectType"];
+            Console.WriteLine("~~~~~COOKIE~~~~~\n" + favProjType);
+
+            HttpContext.Response.Cookies.Append("favoriteProjectType", "Protected String", new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.Now.AddMinutes(1)
+            });
+
             var something = _waterContext.Projects
             .Skip((pageNum - 1) * pageSize)
             .Take(pageSize)
--- backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d2e1b64a3650f9eda0e9513b632197511f0ffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391734ea770dc171fdf46cb63996ce4393a995ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
